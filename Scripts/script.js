aa139166b7bc5b8cba52efcc8164d40eed2e09ef
Taran <Bond_J_0o7@hotmail.com>
2019-06-06 04:57:47 -0400

    only minor animation changes in design process before the the styling is finished.

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,7 +1,28 @@
 function UnSelectAll() {
-			var items = document.getElementsByName('rd');
-			for (var i = 0; i < items.length; i++) {
-					if (items[i].type == 'checkbox')
-							items[i].checked = false;
+	var items = document.getElementsByClassName('uncheck');
+	for (var i = 0; i < items.length; i++) {
+		if (items[i].type == 'checkbox')
+		items[i].checked = false;
+	}
 }
+function UnSelectAll2() {
+	var items = document.getElementsByClassName('uncheck2');
+	for (var i = 0; i < items.length; i++) {
+		if (items[i].type == 'checkbox')
+		items[i].checked = false;
+	}
+}
+function UnSelectAll3() {
+	var items = document.getElementsByClassName('uncheck3');
+	for (var i = 0; i < items.length; i++) {
+		if (items[i].type == 'checkbox')
+		items[i].checked = false;
+	}
+}
+function UnSelectAll4() {
+	var items = document.getElementsByClassName('uncheck4');
+	for (var i = 0; i < items.length; i++) {
+		if (items[i].type == 'checkbox')
+		items[i].checked = false;
+	}
 }
