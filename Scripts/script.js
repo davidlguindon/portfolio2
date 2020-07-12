10837e663dc75b388adfe89429e6a86c48c2f92d
David Guindon <davidlguindon@gmail.com>
2020-07-12 00:55:28 -0400

    nav changes to portfolio2

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,106 +1,106 @@
-// $(".toggle-nav").click(function(){
-//   $(".menu ul").toggleClass('active');
-// });
-//
-// function UnSelectAll() {
-//   var items = document.getElementsByClassName('uncheck');
-//   for (var i = 0; i < items.length; i++) {
-//     if (items[i].type == 'checkbox')
-//     items[i].checked = false;
-//   }
-// }
-// function UnSelectAll2() {
-//   var items = document.getElementsByClassName('uncheck2');
-//   for (var i = 0; i < items.length; i++) {
-//     if (items[i].type == 'checkbox')
-//     items[i].checked = false;
-//   }
-// }
-// function UnSelectAll3() {
-//   var items = document.getElementsByClassName('uncheck3');
-//   for (var i = 0; i < items.length; i++) {
-//     if (items[i].type == 'checkbox')
-//     items[i].checked = false;
-//   }
-// }
-// function UnSelectAll4() {
-//   var items = document.getElementsByClassName('uncheck4');
-//   for (var i = 0; i < items.length; i++) {
-//     if (items[i].type == 'checkbox')
-//     items[i].checked = false;
-//   }
-// }
-// // Get the modal
-// var modal = document.getElementById("myModal");
-//
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-//
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-//
-// // When the user clicks the button, open the modal
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// }
-// function moogle() {
-//   modal.style.display = "block";
-// }
-//
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// }
-//
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// }
-// //Make the DIV element draggagle:
-// dragElement(document.getElementById("modalcontent"));
-//
-// function dragElement(elmnt) {
-//   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-//   if (document.getElementById(elmnt.id + "header")) {
-//     /* if present, the header is where you move the DIV from:*/
-//     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-//   } else {
-//     /* otherwise, move the DIV from anywhere inside the DIV:*/
-//     elmnt.onmousedown = dragMouseDown;
-//   }
-//
-//   function dragMouseDown(e) {
-//     e = e || window.event;
-//     e.preventDefault();
-//     // get the mouse cursor position at startup:
-//     pos3 = e.clientX;
-//     pos4 = e.clientY;
-//     document.onmouseup = closeDragElement;
-//     // call a function whenever the cursor moves:
-//     document.onmousemove = elementDrag;
-//   }
-//
-//   function elementDrag(e) {
-//     e = e || window.event;
-//     e.preventDefault();
-//     // calculate the new cursor position:
-//     pos1 = pos3 - e.clientX;
-//     pos2 = pos4 - e.clientY;
-//     pos3 = e.clientX;
-//     pos4 = e.clientY;
-//     // set the element's new position:
-//     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-//     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-//   }
-//
-//   function closeDragElement() {
-//     /* stop moving when mouse button is released:*/
-//     document.onmouseup = null;
-//     document.onmousemove = null;
-//   }
-// }
+$(".toggle-nav").click(function(){
+  $(".menu ul").toggleClass('active');
+});
+
+function UnSelectAll() {
+  var items = document.getElementsByClassName('uncheck');
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].type == 'checkbox')
+    items[i].checked = false;
+  }
+}
+function UnSelectAll2() {
+  var items = document.getElementsByClassName('uncheck2');
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].type == 'checkbox')
+    items[i].checked = false;
+  }
+}
+function UnSelectAll3() {
+  var items = document.getElementsByClassName('uncheck3');
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].type == 'checkbox')
+    items[i].checked = false;
+  }
+}
+function UnSelectAll4() {
+  var items = document.getElementsByClassName('uncheck4');
+  for (var i = 0; i < items.length; i++) {
+    if (items[i].type == 'checkbox')
+    items[i].checked = false;
+  }
+}
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+function moogle() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+//Make the DIV element draggagle:
+dragElement(document.getElementById("modalcontent"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
 
 function animateIcons () {
   var elements = document.getElementById('bulbOn');
