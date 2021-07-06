8a96ad6a6c3209d338e1c7fd6d8b8a49bdd1f16f
David Guindon <davidlguindon@gmail.com>
2021-07-06 10:30:35 -0400

    Added new case study.

---
+++ Scripts/parallax.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', function(e){
+  const target = document.querySelectorAll('.parallax')
+
+  var scrolled = window.pageYOffset;
+
+  var rate = scrolled * -1.5;
+
+  target[0].style.transform = 'translate3d(0px, '+rate+'px, 0px)';
+});
