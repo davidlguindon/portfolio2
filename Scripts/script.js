e92b3a4bbcc2df6222d4784df6dc65fe3beecda4
David Guindon <davidlguindon@gmail.com>
2021-09-07 07:37:55 -0400

    Changed casestudies page to new version.

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,34 +2,34 @@ $(".toggle-nav").click(function(){
   $(".menu ul").toggleClass('active');
 });
 
-function UnSelectAll() {
-  var items = document.getElementsByClassName('uncheck');
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].type == 'checkbox')
-    items[i].checked = false;
-  }
-}
-function UnSelectAll2() {
-  var items = document.getElementsByClassName('uncheck2');
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].type == 'checkbox')
-    items[i].checked = false;
-  }
-}
-function UnSelectAll3() {
-  var items = document.getElementsByClassName('uncheck3');
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].type == 'checkbox')
-    items[i].checked = false;
-  }
-}
-function UnSelectAll4() {
-  var items = document.getElementsByClassName('uncheck4');
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].type == 'checkbox')
-    items[i].checked = false;
-  }
-}
+// function UnSelectAll() {
+//   var items = document.getElementsByClassName('uncheck');
+//   for (var i = 0; i < items.length; i++) {
+//     if (items[i].type == 'checkbox')
+//     items[i].checked = false;
+//   }
+// }
+// function UnSelectAll2() {
+//   var items = document.getElementsByClassName('uncheck2');
+//   for (var i = 0; i < items.length; i++) {
+//     if (items[i].type == 'checkbox')
+//     items[i].checked = false;
+//   }
+// }
+// function UnSelectAll3() {
+//   var items = document.getElementsByClassName('uncheck3');
+//   for (var i = 0; i < items.length; i++) {
+//     if (items[i].type == 'checkbox')
+//     items[i].checked = false;
+//   }
+// }
+// function UnSelectAll4() {
+//   var items = document.getElementsByClassName('uncheck4');
+//   for (var i = 0; i < items.length; i++) {
+//     if (items[i].type == 'checkbox')
+//     items[i].checked = false;
+//   }
+// }
 // Get the modal
 // var modal = document.getElementById("myModal");
 //
@@ -155,4 +155,10 @@ function callback(entries, observer){
 //     //   entry.time
 //   });
 // };
+})
+$(document).ready(function(){
+var date = new Date();
+var year = date.currentYear = date.getFullYear();
+document.querySelectorAll('sub.date')[0].innerHTML = document.querySelectorAll('sub.date')[0].innerHTML.replace('{{currentYear}}', year);
+console.log('test' + year + '.');
 })
